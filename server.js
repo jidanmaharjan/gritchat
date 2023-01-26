cbef14bfbb3233e6cbe12828cb15b52a9fb4511e
Jidan Maharjan <jidanmaharjan1@gmail.com>
2023-01-26 16:58:59 +0545

    minor bug fixes

--- server.js
+++ server.js
@@ -43,9 +43,9 @@ io.on("connection", (socket) => {
   });
 });
 
-app.use(express.static(path.join(__dirname, "../client/build/")));
+app.use(express.static(path.join(__dirname, "./client/build/")));
 app.get("/*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
 });
 
 server.listen(process.env.PORT || 4000, () => {
